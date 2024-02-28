dc7fd41b0f42a11b8669e88b15d8a288c2590ec4
gigihsigap <gigihsigap@gmail.com>
2024-02-29 02:07:16 +0700

    Fix prod DB config

--- src/config/orm.config.prod.ts
+++ src/config/orm.config.prod.ts
@@ -1,17 +1,17 @@
 import { registerAs } from '@nestjs/config';
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
-import { User } from 'src/user/entities/user.entity';
+import { EmailQueue, FailedQueue, User } from 'src/user/entities/user.entity';
 
 export default registerAs(
   'orm.config',
   (): TypeOrmModuleOptions => ({
     type: 'postgres',
-    host: 'localhost',
-    port: 5432,
-    username: 'postgres',
-    password: 'postgres',
-    database: 'gap',
-    entities: [User],
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT) || 5432,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    entities: [User, EmailQueue, FailedQueue],
     synchronize: false, // Disable this always in production
   }),
 );
